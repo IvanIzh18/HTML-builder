af3c6e7d808dcdc21994a36fd5999b1d479074c3
IvanIzh18 <96039558+IvanIzh18@users.noreply.github.com>
2022-05-23 23:52:05 +0400

    docs: solution added " 02-write-file"

---
+++ 02-write-file/index.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const path = require('path');
+
+const { stdin, stdout } = process;
+
+stdout.write('Доброго дня Вам! Будьте любезны ввести текст.\n');
+
+fs.writeFile(
+    path.join(__dirname, 'text.txt'),
+    '',
+    (err) => {
+        if (err) throw err;
+    }
+);
+
+let text = '';
+stdin.on('data', data => {
+    let dataStr = data.toString();
+
+    if (dataStr.replace('\r\n', '') === 'exit') {
+        process.exit();
+    } else {
+        text += dataStr;
+        fs.appendFile(
+            path.join(__dirname, 'text.txt'), dataStr,
+            err => {
+                if (err) throw err;
+                console.log('Файл был изменен');
+            }
+        );
+
+    };
+
+});
+
+console.log(path.join(__dirname, 'text.txt'));
+
+
+
+process.on('exit', () => stdout.write('Удачи в изучении Node.js!'));
+
+process.on('SIGINT', () => {
+    stdout.write('Удачи в изучении Node.js!');
+    setTimeout(() => process.exit(), 1000);
+});
