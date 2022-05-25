a0a03c10efdcd57fb0dc7224e134794a09319b80
IvanIzh18 <96039558+IvanIzh18@users.noreply.github.com>
2022-05-25 22:54:15 +0400

    docs: corrected the file

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -23,6 +23,17 @@ fs.readdir(oldDirectory, { withFileTypes: true },
 fs.access(directory, (error) => {
     if (error) {
         console.log('Папка "files-copy" не найдена')
+
+        fs.mkdir(path.join(__dirname, 'files-copy'), err => {
+            if (err) throw err;
+            console.log('Папка "files-copy" была создана');
+        });
+        for (let i = 0; i < OldFilesFiltr.length; i++) {
+            fs.copyFile((path.join(oldDirectory, OldFilesFiltr[i])), (path.join(directory, OldFilesFiltr[i])), err => {
+                if (err) throw err; // не удалось скопировать файл/ы
+                console.log('Файл/ы успешно скопирован/ы');
+            });
+        }
     } else {
 
         fs.readdir(directory, { withFileTypes: true },
@@ -34,38 +45,30 @@ fs.access(directory, (error) => {
                         filesFiltr.push(item.name);
                     };
                 };
-                console.log(filesFiltr);
-            })
+                for (let i = 0; i < filesFiltr.length; i++) {
 
-        setTimeout(() => {
+                    fs.unlink((path.join(directory, filesFiltr[i])), (err) => {
+                        if (err) throw err; // не удалось удалить файл
+                        console.log(`${filesFiltr[i]} успешно удален`);
+                    });
+                }
 
-            for (let i = 0; i < filesFiltr.length; i++) {
-
-                fs.unlink((path.join(directory, filesFiltr[i])), (err) => {
-                    if (err) throw err; // не удалось удалить файл
-                    console.log(`${filesFiltr[i]} успешно удален`);
+                fs.rmdir(directory, err => {
+                    if (err) throw err; // не удалось удалить папку
+                    console.log('Папка "files-copy" успешно удаленa');
                 });
 
-            }
+                fs.mkdir(path.join(__dirname, 'files-copy'), err => {
+                    if (err) throw err;
+                    console.log('Папка "files-copy" была создана');
+                });
 
-        }, 50)
-        setTimeout(() => {
-            fs.rmdir(directory, err => {
-                if (err) throw err; // не удалось удалить папку
-                console.log('Папка "files-copy" успешно удаленa');
-            })
-        }, 100)
+                for (let i = 0; i < OldFilesFiltr.length; i++) {
+                    fs.copyFile((path.join(oldDirectory, OldFilesFiltr[i])), (path.join(directory, OldFilesFiltr[i])), err => {
+                        if (err) throw err; // не удалось скопировать файл/ы
+                        console.log('Файл/ы успешно скопирован/ы');
+                    });
+                }
+            });
     };
 });
-setTimeout(() => {
-    fs.mkdir(path.join(__dirname, 'files-copy'), err => {
-        if (err) throw err;
-        console.log('Папка "files-copy" была создана');
-    });
-    for (let i = 0; i < OldFilesFiltr.length; i++) {
-        fs.copyFile((path.join(oldDirectory, OldFilesFiltr[i])), (path.join(directory, OldFilesFiltr[i])), err => {
-            if (err) throw err; // не удалось скопировать файл/ы
-            console.log('Файл/ы успешно скопирован/ы');
-        });
-    }
-}, 200)
